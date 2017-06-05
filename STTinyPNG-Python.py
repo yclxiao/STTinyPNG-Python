92b4e6354c351a672f04e3cdf0f937407c4e26a5
OF1089（杨成龙） <yangchenglong@qianmi.com>
2017-06-05 14:51:14 +0800

    修改批量压缩文件

--- STTinyPNG-Python.py
+++ STTinyPNG-Python.py
@@ -1,23 +1,37 @@
+#-*- encoding: UTF-8 -*-
+
 import tinify
 import os
 import os.path
 
-tinify.key = "your AppKey" # AppKey
-fromFilePath = "/Users/tangjr/Desktop/test1" # 源路径
-toFilePath = "/Users/tangjr/Desktop/test2" # 输出路径
+#tiny官网注册的key
+tinify.key = "saO2EtqhoHCfVlbYxHvf-NITmTNX2sXr"
 
+#文件源路径
+fromFilePath = "/Users/yclxiao/Project/python/STTinyPNG-Python/big"
+#文件目标路径
+toFilePath = "/Users/yclxiao/Project/python/STTinyPNG-Python/bignew"
+
+#遍历每一层路径  root是指遍历的文件目录
 for root, dirs, files in os.walk(fromFilePath):
 	for name in files:
 		fileName, fileSuffix = os.path.splitext(name)
 		if fileSuffix == '.png' or fileSuffix == '.jpg':
+
+			print root
+
 			toFullPath = toFilePath + root[len(fromFilePath):]
 			toFullName = toFullPath + '/' + name
 
-			if os.path.isdir(toFullPath):
+			fromFullPath = fromFilePath + root[len(fromFilePath):]
+			fromFullName = fromFullPath + '/' + name
+
+			#如果目录不存在则创建
+			if os.path.exists(toFullPath):
 				pass
 			else:
 				os.mkdir(toFullPath)
-			
-			with open(toFullName, 'rb') as source:
-			    source_data = source.read()
-			    result_data = tinify.from_buffer(source_data).to_buffer()
+
+			#利用tiny的的api进行图片压缩
+			source = tinify.from_file(fromFullName)
+			source.to_file(toFullName)
